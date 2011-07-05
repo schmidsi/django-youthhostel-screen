2b0886b5614977733416f2b6f40413fa4a5e0927
Simon Schmid <ssc@feinheit.ch>
2011-07-05 23:28:41 +0200

    first contents dynamic + image slideshow

---
+++ screen/contents.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.template.loader import render_to_string
+
+class AnnouncementContent(models.Model):
+    announcement = models.CharField('Announcement', max_length=200)
+    
+    class Meta:
+        abstract = True
+        verbose_name = u'Ankündigung'
+        verbose_name_plural = u'Ankündigungen'
+    
+    def render(self, **kwargs):
+        return self.announcement
