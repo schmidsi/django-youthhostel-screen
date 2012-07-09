d36c8e36c3d6a695353caeed0eef4b53ad5a5373
Simon Schmid <ssc@feinheit.ch>
2012-07-09 17:43:16 +0200

    adding weather template tags

---
+++ screen/templatetags/weather.py
@@ -0,0 +1,35 @@
+from django import template
+
+from feincms_oembed.models import CachedLookup
+
+from lxml import etree
+
+register = template.Library()
+
+
+@register.inclusion_tag('widgets/weather.html', takes_context=True)
+def weather_widget(context, city_search):
+    raw = CachedLookup.objects.request('http://www.google.com/ig/api?weather=%s' % city_search, max_age=10*60)
+    xmlroot = etree.XML(raw)
+    xmlweather = xmlroot.find('weather')
+
+    weather = {
+        'current' : {
+            'temp' : xmlweather.find('current_conditions').find('temp_c').attrib['data'],
+            'icon' : xmlweather.find('current_conditions').find('icon').attrib['data'].split('/')[-1],
+            'humidity' : xmlweather.find('current_conditions').find('humidity').attrib['data'].split(' ')[-1],
+        },
+        'forecast' : [],
+    }
+
+    for xmlforecast in xmlweather.findall('forecast_conditions'):
+        weather['forecast'].append({
+            'day' : xmlforecast.find('day_of_week').attrib['data'],
+            'high' : (int(xmlforecast.find('high').attrib['data']) - 32) * 5 / 9,
+            'low' : (int(xmlforecast.find('low').attrib['data']) - 32) * 5 / 9,
+            'icon' : xmlforecast.find('icon').attrib['data'].split('/')[-1],
+        })
+
+    context['weather'] = weather
+
+    return context
