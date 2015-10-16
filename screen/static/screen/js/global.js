2570149226a25cacbbc14e53e5a965890b4bf6e4
Simon Schmid <simon@schmidsi.ch>
2015-10-16 18:07:22 +0200

    use skycons

--- screen/static/screen/js/global.js
+++ screen/static/screen/js/global.js
@@ -1,98 +1,11 @@
-var timeouts = {};
+$(function() {
+  var skycons = new Skycons({color: 'rgb(80, 145, 205)'})
 
-$(function(){
-    getNextContent('main', '#content');
-    getNextContent('announcements', '#announcements');
-    
-    $('.ticker').screenSlide({'visible' : 8000});
-    
-    var weatherInterval = window.setInterval(function(){
-        $('#weather').load('/ajax/weather/');
-    }, 30*60*1000);
-    
-    var tickerInterval = window.setInterval(function(){
-        $('#ticker').load('/ajax' + $('#container').data('path') + '?region=ticker&content=all', function(){
-            $('.ticker').screenSlide({'visible' : 8000});
-        });
-    }, 30*60*1000);
-});
+  skycons.add('weather-icon', Skycons.PARTLY_CLOUDY_DAY)
+  skycons.play()
+})
 
-function getNextContent(region, container) {
-    var $container = $(container);
-    $.ajax({
-        type : 'GET',
-        url : '/ajax' + $('#container').data('path'),
-        cache : false,
-        data : {
-            region : region,
-            last : $container.data('currentcontent'),
-            content : 'random'
-        },
-        dataType : 'html',
-        success: function(data, status, jqXHR) {
-            var $data = $(data)
-            var duration = $data.data('duration');
-            var oldContent = $container.children().fadeOut(500);
-            
-            if (duration == undefined || duration < 1) {
-                duration = 1;
-            }
-            
-            $container.append($data);
-
-            if ($data.hasClass('youtube')) {
-                initYoutube($data, region, container);
-            } else if ($data.hasClass('facebook')) {
-                getFacebookPost($data, region, container);
-            }
-
-            $data.hide().fadeIn(500);
-            $container.data('currentcontent', $data.data('id'));
-            
-            window.setTimeout(function() { oldContent.remove() }, 5000);
-            
-            timeouts[region] = window.setTimeout(getNextContent, duration * 1000, region, container)
-        },
-        error: function(jqXHR, status, error) {
-            /* silently retry to get content after 10 sec */
-            var updateTimeout = window.setTimeout(getNextContent, 
-                10 * 1000, region, container)
-        }
-    })
-}
-
-function initYoutube($el, region, container) {
-    var player = new YT.Player($el.get(0), {
-        height: $el.height(),
-        width: $el.width(),
-        videoId: $el.data('youtubeid'),
-        playerVars: { 'autoplay': 1, 'controls': 0 },
-        events: {
-            'onReady': function(event) {
-                event.target.playVideo();
-                event.target.mute();
-
-                // emergency exit: if the video has 3 time longer than its
-                // duration to play (due to network errors, etc), go to next content
-                window.clearTimeout(timeouts[region]);
-                timeouts[region] = window.setTimeout(
-                    getNextContent,
-                    event.target.getDuration() * 3000,
-                    region,
-                    container
-                )
-            },
-            'onStateChange': function(event) {
-                if (event.data == YT.PlayerState.ENDED) {
-                    window.clearTimeout(timeouts[region]);
-                    getNextContent(region, container);
-                }
-            }
-        }
-    });
-}
-
-
+/*
 function getFacebookPost($el, region, container) {
     var user = $el.data('user'),
         url = 'https://graph.facebook.com/' + user + '/posts/?fields=type,message,object_id&filter=type:photo&access_token=159457810754902|UqvcQEszzDvlLlft85FtuP2LWYo';
@@ -111,3 +24,4 @@ function getFacebookPost($el, region, container) {
 
     });
 }
+*/
